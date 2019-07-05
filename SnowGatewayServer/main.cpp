cd990f1058a303e0bf3beca343317750b878afa9
Snow <polarpenguin@foxmail.com>
2019-07-05 17:36:27 +0800

    Update version number.

--- SnowGatewayServer/main.cpp
+++ SnowGatewayServer/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
     QApplication serverApp(argc, argv);
     QApplication::setApplicationName("SnowHomeGateway-Server");
-    QApplication::setApplicationVersion("0.0.1");
+    QApplication::setApplicationVersion("0.1.0");
     QApplication::setOrganizationDomain("xuefeng.space");
 
     SnowMainFrame mainFrame;
